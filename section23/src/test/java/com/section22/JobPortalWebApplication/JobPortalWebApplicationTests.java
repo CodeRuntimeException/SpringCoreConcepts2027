483157574d6374b261962bbb9ea261ad0adf10e3
pranay <pranay@gmail.com>
2026-08-24 07:17:20 -0700

    Getting Started With SpringBoot

---
+++ section23/src/test/java/com/section22/JobPortalWebApplication/JobPortalWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.section22.JobPortalWebApplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobPortalWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
